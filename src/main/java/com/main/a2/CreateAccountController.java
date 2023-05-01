41eed9ecd2ba8a874a0e40f69e9da4c6dff3cf51
Sevgi <christurner2384@gmail.com>
2023-05-01 22:55:44 +1000

    create account and log in

--- src/main/java/com/main/a2/CreateAccountController.java
+++ src/main/java/com/main/a2/CreateAccountController.java
@@ -5,8 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,10 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class CreateAccountController {
     private Stage stage;
@@ -40,7 +36,7 @@ public class CreateAccountController {
     private TextField fieldCreateLastName;
 
     @FXML
-    private TextField fieldCreatePassword;
+    private PasswordField fieldCreatePassword;
 
     @FXML
     private TextField fieldCreateStudentId;
@@ -69,17 +65,56 @@ public class CreateAccountController {
             lastName = fieldCreateLastName.getText();
             password = fieldCreatePassword.getText();
 
-            String query = "INSERT INTO students VALUES ('"+username+"', '"+studentId+"', '"+firstName+"', '"+lastName+"', '"+password+"'" +
-                    ")";
-            System.out.println(query);
-            Statement state = conn.createStatement();
-            state.executeUpdate(query);
-            state.close();
-            conn.close();
+            if (!ExistingUsername(conn, username) && !ExistingStudentId(conn, studentId)) {
+
+                String query = "INSERT INTO students VALUES ('" + username + "', '" + studentId + "', '" + firstName + "', '" + lastName + "', '" + password + "'" +
+                        ")";
+                System.out.println(query);
+                Statement state = conn.createStatement();
+                state.executeUpdate(query);
+                state.close();
+                conn.close();
+            }
+
+            else {
+                System.out.println("Details already exist!");
+            }
         }
         catch (Exception e) {
             System.out.println("Couldn't connect: " + e);
         }
+    }
+
+    public boolean ExistingUsername(Connection conn, String username) throws SQLException {
+        String query = "SELECT * FROM students";
+        Statement state = conn.createStatement();
+        ResultSet rs = state.executeQuery(query);
+
+        while (rs.next()) {
+            System.out.println(rs.getString("username"));
+            if (rs.getString("username").equals(username)) {
+                System.out.println("existing username, returning true");
+                return true;
+            }
+        }
+        System.out.println("returning false");
+        return false;
+    }
+
+    public boolean ExistingStudentId(Connection conn, String username) throws SQLException {
+        String query = "SELECT * FROM students";
+        Statement state = conn.createStatement();
+        ResultSet rs = state.executeQuery(query);
+
+        while (rs.next()) {
+            System.out.println(rs.getString("student_id"));
+            if (rs.getString("student_id").equals(studentId)) {
+                System.out.println("existing id, returning true");
+                return true;
+            }
+        }
+        System.out.println("returning false");
+        return false;
     }
 
 
--- src/main/java/com/main/a2/LogInController.java
+++ src/main/java/com/main/a2/LogInController.java
@@ -4,15 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class LogInController {
     private Stage stage;
+    private Connection conn;
+    private Statement state;
+    private String username;
+    private String password;
 
     @FXML
     private Button btnCreateAccount;
@@ -21,19 +28,19 @@ public class LogInController {
     private Button btnSignIn;
 
     @FXML
-    private TextField fieldPassword;
+    private PasswordField fieldPassword;
 
     @FXML
     private TextField fieldUsername;
 
-    public void LogInScene(ActionEvent event) throws IOException {
+    /*public void LogInScene(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LogIn.class.getResource("CourseList.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load(), 1110, 517);
         stage.setTitle("myTimetable - Course Enrollment!");
         stage.setScene(scene);
         stage.show();
-    }
+    }*/
 
     public void CreateAccountScene(ActionEvent event) throws  IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LogIn.class.getResource("CreateAccount.fxml"));
@@ -42,6 +49,50 @@ public class LogInController {
         stage.setTitle("myTimetable - Create Account");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void LogIn(ActionEvent event) throws SQLException, IOException {
+        System.out.println("Logging in, opening DB");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:src\\database\\mytimetable.db");
+        username = fieldUsername.getText();
+        password = fieldPassword.getText();
+
+        if (VerifyPassword(conn, password) && VerifyUsername(conn, username)) {
+            FXMLLoader fxmlLoader = new FXMLLoader(LogIn.class.getResource("CourseList.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(fxmlLoader.load(), 1110, 517);
+            stage.setTitle("myTimetable - Course Enrollment!");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    public boolean VerifyUsername(Connection conn, String username) throws SQLException {
+        String query = "SELECT * FROM students";
+        state = conn.createStatement();
+        ResultSet rs = state.executeQuery(query);
+
+        while (rs.next()) {
+            if (rs.getString("username").equals(username)) {
+                System.out.println("valid username, returning true");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean VerifyPassword(Connection conn, String password) throws SQLException {
+        String query = "SELECT * FROM students";
+        state = conn.createStatement();
+        ResultSet rs = state.executeQuery(query);
+
+        while (rs.next()) {
+            if (rs.getString("password").equals(password)) {
+                System.out.println("valid pass, returning true");
+                return true;
+            }
+        }
+        return false;
     }
 
 }
